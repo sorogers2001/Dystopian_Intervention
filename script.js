f50cf44a0c200200b1f8b07f97bf30a0fa61d1b7
sorogers2001 <sorogers@ucsd.edu>
2026-01-27 23:41:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
 
   gsap.registerPlugin(ScrollTrigger);
 
-  const images = gsap.utils.toArray(".bg-image");
+  const images = gsap.utils.toArray(".image");
   const fadeOverlay = document.querySelector(".fade-to-black");
   const audio = document.getElementById("static-audio");
   const introText = document.querySelector(".intro-text");
@@ -10,14 +10,12 @@ window.addEventListener("load", () => {
   const outroBottom = document.querySelector(".outro-bottom");
 
   const numImages = images.length;
-  const fadeDuration = 0.15; // last 15% scroll
+  const fadeDuration = 0.15; // last 15% of scroll
 
-  // Load backgrounds from data-src
-  images.forEach(div => {
-    div.style.backgroundImage = `url(${div.dataset.src})`;
-  });
+  // Show first image initially
+  images.forEach((img,i) => img.style.opacity = i===0 ? 1 : 0);
 
-  // Fade in audio
+  // Fade in audio automatically
   gsap.to(audio, { volume:0.1, duration:3, ease:"none" });
 
   ScrollTrigger.create({
@@ -28,15 +26,15 @@ window.addEventListener("load", () => {
     onUpdate: self => {
       const progress = self.progress;
 
-      // --- Intro fade ---
+      // --- Intro text fade (first 10%) ---
       introText.style.opacity = progress < 0.1 ? 1 - progress/0.1 : 0;
 
-      // --- Images crossfade + zoom ---
+      // --- Image crossfade + zoom ---
       const imageProgress = Math.min(progress, 1 - fadeDuration);
       const index = Math.floor(imageProgress * numImages);
       const localProgress = (imageProgress * numImages) - index;
 
-      images.forEach((img, i) => {
+      images.forEach((img,i) => {
         if(i === index){
           img.style.opacity = 1;
           img.style.transform = `scale(${1 + 0.3 * localProgress})`;
@@ -46,7 +44,7 @@ window.addEventListener("load", () => {
         }
       });
 
-      // --- Last image fade + outro ---
+      // --- Last image fade + fade-to-black + outro ---
       if(progress >= 1 - fadeDuration){
         const fadeProgress = (progress - (1 - fadeDuration)) / fadeDuration;
 
@@ -56,6 +54,7 @@ window.addEventListener("load", () => {
         fadeOverlay.style.opacity = fadeProgress;
         outroMain.style.opacity = fadeProgress;
         outroBottom.style.opacity = fadeProgress;
+
       } else {
         fadeOverlay.style.opacity = 0;
         outroMain.style.opacity = 0;
